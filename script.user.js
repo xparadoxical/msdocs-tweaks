2de6c01956ca5636d4b228354e153e123c38d289
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2022-10-05 00:59:51 +0200

    docs.microsoft.com -> learn.microsoft.com

--- script.user.js
+++ script.user.js
@@ -1,24 +1,27 @@
 // ==UserScript==
 // @name         MSDocs tweaks
 // @author       xparadoxical
-// @version      1.1.1
+// @version      1.1.2
 // @description  Redirects to the en-us version of the current msdocs page and expands the document outline ("In this article")
 // @source       https://github.com/xparadoxical/msdocs-tweaks
 // @updateURL    https://github.com/xparadoxical/msdocs-tweaks/raw/main/script.user.js
 // @downloadURL  https://github.com/xparadoxical/msdocs-tweaks/raw/main/script.user.js
 // @grant        none
 // @run-at       document-start
-// @match        https://docs.microsoft.com/*
+// @match        https://learn.microsoft.com/*
 // ==/UserScript==
+
+//en-us
 let pathname = window.location.pathname.split('/');
 if (pathname[1].toLowerCase() !== 'en-us') {
     pathname[1] = 'en-us';
     pathname = pathname.join('/');
     window.location.href = window.location.origin + pathname + window.location.search;
 } else {
+    //auto-expand
     window.addEventListener('load', (event) => {
         document.querySelector('nav#side-doc-outline button[data-show-more]').click();
-        document.activeElement.blur();
+        document.activeElement.blur/*unfocus*/();
 
         const hash = window.location.hash;
         document.querySelector("[id='" + hash.substring(1) + "']").scrollIntoView();
